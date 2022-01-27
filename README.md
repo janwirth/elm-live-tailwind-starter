56e4d4de4398390d7b2f0340f608bfff9ab19d35
Jan Wirth <jan@field33.com>
2022-01-27 09:49:07 +0100

    Init

---
+++ README.md
@@ -0,0 +1,9 @@
+# elm-live-tailwind-starter
+
+How do I get started?
+```
+git clone git@github.com:janwirth/elm-live-tailwind-starter.git
+cd elm-live-tailwind-starter
+npm i
+npm start
+```
---
+++ src/Main.elm
@@ -0,0 +1,7 @@
+module Main exposing (main)
+import Html
+import Html.Attributes
+
+main = Html.div [
+        Html.Attributes.class "bg-gray-800 m-4 rounded-md text-gray-100 p-4"
+    ] [Html.text "Welcome to elm-live-tailwind-starter"]
